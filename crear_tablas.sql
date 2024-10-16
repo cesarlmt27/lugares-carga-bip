32849fee7298cc62dee8793b6a60b5624a50e589
César Montero <72035439+cesarlmt27@users.noreply.github.com>
2024-10-16 17:30:44 -0300

    Movidos

--- crear_tablas.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS nodos (
-    id SERIAL PRIMARY KEY,
-    direccion VARCHAR(255),
-    longitud DOUBLE PRECISION,
-    latitud DOUBLE PRECISION,
-    geom GEOMETRY(Point, 4326)
-);
