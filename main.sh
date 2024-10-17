19865d5f46ea06da3dff1fcde00dcb63f608fe95
César Montero <72035439+cesarlmt27@users.noreply.github.com>
2024-10-17 03:35:16 -0300

    En proceso

--- main.sh
+++ main.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-cd sql
+set -x
 
-python3 ejecutar_sql.py
-
-python3 seed.py
-
-cd ..
-
-cd infraestructura
-
+pushd infraestructura
+python3 datosgob.py
+python3 nodos.py
 python3 caminos.py
+popd
+
+pushd metadata
+python3 cajeros.py
+python3 informacion.py
+python3 saldo_bip.py
+popd
+
+pushd amenazas
+python3 atropellos.py
+python3 feriados.py
+python3 robos.py
+popd
+
+pushd sql
+python3 ejecutar_sql.py
+python3 seed.py
+popd
+
+set +x
