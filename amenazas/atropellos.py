050b47b055630ffc779f40cbdfeda6295301f090
César Montero <72035439+cesarlmt27@users.noreply.github.com>
2024-10-16 23:07:03 -0300

    Cambios revertidos

--- amenazas/atropellos.py
+++ amenazas/atropellos.py
@@ -1,5 +1,4 @@
 import requests
-import os
 
 # URL de la API REST con el formato GeoJSON
 url = "https://services3.arcgis.com/vaJl1B5HEzZj7154/arcgis/rest/services/Atropellos_RM_2022/FeatureServer/0/query"
@@ -17,12 +16,9 @@ response = requests.get(url, params=params)
 
 # Verificar si la solicitud fue exitosa
 if response.status_code == 200:
-    # Crear el directorio "modificados" si no existe
-    os.makedirs('modificados', exist_ok=True)
-    
-    # Guardar el contenido de la respuesta en un archivo GeoJSON en el directorio "modificados"
-    with open(os.path.join('modificados', 'atropellos_rm_2022.geojson'), 'w') as file:
+    # Guardar el contenido de la respuesta en un archivo GeoJSON
+    with open('atropellos_rm_2022.geojson', 'w') as file:
         file.write(response.text)
-    print("Archivo GeoJSON descargado correctamente en el directorio 'modificados'.")
+    print("Archivo GeoJSON descargado correctamente.")
 else:
     print(f"Error al descargar los datos. Código de estado: {response.status_code}")
--- amenazas/feriados.py
+++ amenazas/feriados.py
@@ -1,6 +1,5 @@
 import requests
 import json
-import os
 from time import sleep
 
 # URL de la API de feriados nacionales
@@ -25,14 +24,11 @@ for attempt in range(max_retries):
             # Convertir la respuesta en JSON
             feriados_data = response.json()
             
-            # Crear el directorio "modificados" si no existe
-            os.makedirs('modificados', exist_ok=True)
-            
-            # Guardar los datos en un archivo JSON en el directorio "modificados"
-            with open(os.path.join('modificados', 'feriados_nacionales.json'), 'w', encoding='utf-8') as file:
+            # Guardar los datos en un archivo JSON
+            with open('feriados_nacionales.json', 'w', encoding='utf-8') as file:
                 json.dump(feriados_data, file, indent=4, ensure_ascii=False)
             
-            print("Datos de feriados nacionales guardados correctamente en el directorio 'modificados'.")
+            print("Datos de feriados nacionales guardados correctamente en feriados_nacionales.json.")
             break  # Salir del bucle si la solicitud fue exitosa
         else:
             print(f"Error al descargar los datos. Código de estado: {response.status_code}")
