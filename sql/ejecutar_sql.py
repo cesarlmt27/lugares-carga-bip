1f48f4e6d29584432032edcbbd5a17d81d53f861
César Montero <72035439+cesarlmt27@users.noreply.github.com>
2024-11-13 20:09:56 -0300

    Ejecución de archivo .sql al pasarlo como argumento

--- sql/ejecutar_sql.py
+++ sql/ejecutar_sql.py
@@ -1,4 +1,5 @@
 import psycopg
+import sys
 
 # Función para ejecutar un archivo .sql
 def execute_sql_file(file_path):
@@ -13,12 +14,15 @@ def execute_sql_file(file_path):
 conn = psycopg.connect("dbname=postgres user=postgres password=kj2aBv6f33cZ host=db port=5432")
 cur = conn.cursor()
 
-# Crear extensiones
-cur.execute("CREATE EXTENSION postgis;")
-cur.execute("CREATE EXTENSION pgrouting;")
+# Obtener el path del archivo .sql desde los argumentos de la línea de comandos
+if len(sys.argv) != 2:
+    print("Uso: python ejecutar_sql.py <path_del_archivo_sql>")
+    sys.exit(1)
+
+sql_file_path = sys.argv[1]
 
 # Llamar a la función con la ruta del archivo .sql
-execute_sql_file('crear_tablas.sql')
+execute_sql_file(sql_file_path)
 
 # Hacer los cambios persistentes en la base de datos
 conn.commit()
